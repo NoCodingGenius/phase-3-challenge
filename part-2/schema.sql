889bcb32874aa40887563b06e6e32d22d065e126
LaToya Williams <platinumtoii@yahoo.com>
2017-09-18 14:01:12 -0700

    All files are stored under the part-2/ filder. Database schema (all CREATE TABLE statements) is defined in a file schema.sql.

---
+++ part-2/schema.sql
@@ -0,0 +1,26 @@
+DROP TABLE IF EXISTS grocery_items CASCADE;
+DROP TABLE IF EXISTS shoppers CASCADE;
+DROP TABLE IF EXISTS orders CASCADE;
+DROP TABLE IF EXISTS order_items CASCADE;
+
+CREATE TABLE grocery_items (
+  id SERIAL PRIMARY KEY,
+  name TEXT NOT NULL,
+  price TEXT NOT NULL,
+  section TEXT NOT NULL
+);
+
+CREATE TABLE shoppers (
+  id SERIAL PRIMARY KEY,
+  name TEXT NOT NULL
+);
+
+CREATE TABLE orders (
+  id SERIAL PRIMARY KEY,
+  shopper_id INTEGER REFERENCES shopper(id)
+);
+
+CREATE TABLE order_items (
+  item_id INTEGER REFERENCES grocery_items(id),
+  order_id INTEGER REFERENCES orders(id)
+);
