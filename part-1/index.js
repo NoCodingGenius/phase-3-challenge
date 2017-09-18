1786486822ce8fa24934fd057c4c2099bf993be5
LaToya Williams <platinumtoii@yahoo.com>
2017-09-18 10:56:56 -0700

    Route 2: POST /api/array/concat. The response is determined by parsing the body params.

--- part-1/index.js
+++ part-1/index.js
@@ -1,6 +1,9 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 const app = express();
 const PORT = 3000;
+
+app.use(bodyParser.json());
 
 const daysOfWeek = {
   monday: 1,
@@ -14,13 +17,26 @@ const daysOfWeek = {
 
 app.get('/api/days/:day', function (req, res) {
   const day = req.params.day;
+
   for (let key in daysOfWeek) {
     if (key === day) {
       res.send(String(daysOfWeek[`${day}`]));
     }
   }
   res.status(400).send(`${day} is not a valid day!`)
-})
+});
+
+app.post('/api/array/concat', function (req, res) {
+  const array1 = req.body.array1;
+  const array2 = req.body.array2;
+  const result = array1.concat(array2);
+
+  if (!Array.isArray(array1) || !Array.isArray(array2)) {
+    res.status(400).send({"error": "Input data should be of type Array"});
+  } else {
+    res.send(result);
+  }
+});
 
 app.listen(PORT, function() {
   console.log(`Listening on port ${PORT}!`)
