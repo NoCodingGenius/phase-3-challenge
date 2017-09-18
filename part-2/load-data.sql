c11d417888bf818dad33cea3034c5e63be019561
LaToya Williams <platinumtoii@yahoo.com>
2017-09-18 14:30:49 -0700

    SQL statements to nsert data into the orders and shoppers table is added to the file load-data.sql. Add at least 5 rows in each table).

--- part-2/load-data.sql
+++ part-2/load-data.sql
@@ -1,2 +1,8 @@
 COPY grocery_items (name, price, section)
 FROM '/Users/latoyawilliams/LGProjects/Phase-2/phase-3-challenge/part-2/grocerySeedData.csv' DELIMITER ',' CSV HEADER;
+
+INSERT INTO shoppers (name)
+  VALUES ('Sage'), ('Journey'), ('Willow'), ('Lyric'), ('Love');
+
+INSERT INTO orders (shopper_id)
+  VALUES ('1'), ('3'), ('5'), ('3'), ('4');
