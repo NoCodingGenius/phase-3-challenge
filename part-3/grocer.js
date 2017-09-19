550258a3d669192fbb45902cca72cf1bcdb40666
LaToya Williams <latoyawilliams@LaToyas-MacBook-Pro.local>
2017-09-19 14:58:39 -0700

    Clicking on the Clear button in the cart modal removes all items from the cart.

--- part-3/grocer.js
+++ part-3/grocer.js
@@ -1,6 +1,9 @@
 const modal = document.getElementsByClassName('modal')[0];
 const openModal = document.getElementById('cart-button');
 const closeModal = document.getElementsByClassName('close-modal')[0];
+const clearModal = document.getElementsByClassName('clear-modal')[0];
+const divContent = document.getElementsByClassName('modal-item-name')[0];
+const div2Content = document.getElementsByClassName('modal-item-price')[0];
 
 const addToCartButton = document.getElementsByClassName('add-to-cart');
 const cartItemCount = document.getElementById('cart-item-count');
@@ -23,20 +26,18 @@ function addToCart(addToCartButton) {
   cartItemNames.push(itemName);
   cartItemPrices.push(itemPrice);
 
-  let div = document.getElementsByClassName('modal-item-name')[0];
   let divElement = document.createElement('div');
 
   for (var i = 0; i < cartItemNames.length; i++) {
     divElement.textContent = cartItemNames[i];
-    div.appendChild(divElement);
+    divContent.appendChild(divElement);
   }
 
-  let div2 = document.getElementsByClassName('modal-item-price')[0];
   let div2Element = document.createElement('div');
 
   for (var i = 0; i < cartItemPrices.length; i++) {
     div2Element.textContent = cartItemPrices[i];
-    div2.appendChild(div2Element);
+    div2Content.appendChild(div2Element);
   }
 }
 
@@ -47,4 +48,16 @@ openModal.addEventListener('click', function () {
 
 closeModal.addEventListener('click', function () {
   modal.style.display = 'none';
+})
+
+//Clear Cart
+clearModal.addEventListener('click', function () {
+  while (divContent.firstChild && div2Content.firstChild) {
+    divContent.firstChild.remove();
+    div2Content.firstChild.remove();
+  }
+  itemCount = 0;
+  cartItemCount.innerHTML = `(${itemCount})`;
+  cartItemNames = [];
+  cartItemPrices = [];
 })
