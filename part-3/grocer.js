4d69e87cba778b9a2b7fdde4342e18d31a06ddc7
LaToya Williams <platinumtoii@yahoo.com>
2017-09-19 13:03:10 -0700

    Clicking on the X button in the cart modal closes the modal.

--- part-3/grocer.js
+++ part-3/grocer.js
@@ -1,3 +1,7 @@
+const modal = document.getElementsByClassName('modal')[0];
+const openModal = document.getElementById('cart-button');
+const closeModal = document.getElementsByClassName('close-modal')[0];
+
 const addToCartButton = document.getElementsByClassName('add-to-cart');
 const cartItemCount = document.getElementById('cart-item-count');
 let itemCount = 0;
@@ -11,3 +15,12 @@ function addToCart() {
   itemCount++;
   cartItemCount.innerHTML = `(${itemCount})`;
 }
+
+//Cart Modal
+openModal.addEventListener('click', function () {
+  modal.style.display = 'block';
+})
+
+closeModal.addEventListener('click', function () {
+  modal.style.display = 'none';
+})
