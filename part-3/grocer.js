5a17aba073a9a9b9e79dddc9c5bfc77e7b31b45a
LaToya Williams <latoyawilliams@LaToyas-MacBook-Pro.local>
2017-09-19 14:09:31 -0700

    Clicking on the cart button will show the cart modal with a list of all items added.

--- part-3/grocer.js
+++ part-3/grocer.js
@@ -5,15 +5,39 @@ const closeModal = document.getElementsByClassName('close-modal')[0];
 const addToCartButton = document.getElementsByClassName('add-to-cart');
 const cartItemCount = document.getElementById('cart-item-count');
 let itemCount = 0;
+let cartItemNames = [];
+let cartItemPrices = [];
 
 //Add To Cart
 for (var i = 0; i < addToCartButton.length; i++) {
   addToCartButton[i].addEventListener('click', addToCart);
 };
 
-function addToCart() {
+function addToCart(addToCartButton) {
   itemCount++;
   cartItemCount.innerHTML = `(${itemCount})`;
+
+  let element = addToCartButton.target
+  let itemPrice = element.previousElementSibling.innerHTML;
+  let itemName = element.previousElementSibling.previousElementSibling.innerHTML;
+  cartItemNames.push(itemName);
+  cartItemPrices.push(itemPrice);
+
+  let div = document.getElementsByClassName('modal-item-name')[0];
+  let divElement = document.createElement('div');
+
+  for (var i = 0; i < cartItemNames.length; i++) {
+    divElement.textContent = cartItemNames[i];
+    div.appendChild(divElement);
+  }
+
+  let div2 = document.getElementsByClassName('modal-item-price')[0];
+  let div2Element = document.createElement('div');
+
+  for (var i = 0; i < cartItemPrices.length; i++) {
+    div2Element.textContent = cartItemPrices[i];
+    div2.appendChild(div2Element);
+  }
 }
 
 //Cart Modal
