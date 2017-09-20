701f38564025a01388b0070b82ee3421ea31e8e9
LaToya Williams <latoyawilliams@LaToyas-MacBook-Pro.local>
2017-09-19 21:32:57 -0700

    Resolve the error: can;t set headers after they are sent, with a return statement. This is a recommended good practice from stackoverflow.

--- part-1/index.js
+++ part-1/index.js
@@ -15,30 +15,29 @@ const daysOfWeek = {
   sunday: 7
 };
 
-app.get('/api/days/:day', function (req, res) {
+app.get('/api/days/:day', function(req, res) {
   const day = req.params.day;
 
   for (let key in daysOfWeek) {
-    if (!key === day) {
-      res.status(400).send(`${day} is not a valid day!`);
-    } else {
-      res.send(String(daysOfWeek[`${day}`]));
+    if (key === day) {
+      return res.send(String(daysOfWeek[`${day}`]));
     }
-  };
+  }
+  res.status(400).send(`${day} is not a valid day!`);
 });
 
-app.post('/api/array/concat', function (req, res) {
-  const array1 = req.body.array1;
-  const array2 = req.body.array2;
-  const result = array1.concat(array2);
+app.post('/api/array/concat', function(req, res) {
+const array1 = req.body.array1;
+const array2 = req.body.array2;
+const result = array1.concat(array2);
 
-  if (!Array.isArray(array1) || !Array.isArray(array2)) {
-    res.status(400).send({"error": "Input data should be of type Array"});
-  } else {
-    res.send({"result": result});
-  }
+if (!Array.isArray(array1) || !Array.isArray(array2)) {
+  res.status(400).send({"error": "Input data should be of type Array"});
+} else {
+  res.send({"result": result});
+}
 });
 
 app.listen(PORT, function() {
-  console.log(`Listening on port ${PORT}!`)
+console.log(`Listening on port ${PORT}!`)
 })
