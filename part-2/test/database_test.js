07589963a18a1674bafabe2f39fb76c931959957
LaToya Williams <platinumtoii@yahoo.com>
2017-09-20 10:13:56 -0700

    Write an additional test with Moch and Chai in database_test.js that assert the databse function for the command product-list, shopper-orders, and real-shoppers.

--- part-2/test/database_test.js
+++ part-2/test/database_test.js
@@ -7,18 +7,21 @@ describe('Queries', function () {
     return queries.productList('dairy')
       .then(function (results) {
         expect(results[2].product_name).to.equal('Cream Cheese');
+        expect(results).to.have.lengthOf(7);
       })
   });
   it('shopperOrders', function (){
     return queries.shopperOrders('3')
       .then(function (results) {
         expect(results[1].total_cost).to.equal('10.81');
+        expect(results).to.be.a('Array');
       })
   });
   it('realShoppers', function (){
     return queries.realShoppers()
       .then(function (results) {
         expect(results[3].number_of_orders).to.equal('1');
+        expect(results[2].shopper_name).to.be.a('String');
       })
   });
 });
